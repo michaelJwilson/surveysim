Factored out some methods into utils.py, prioritizes the tiles in terms of DEC, changed airmass calculation, corrected some bugs.

py/surveysim/afternoonplan.py
@@ -2,11 +2,13 @@ import numpy as np
 import astropy.io.fits as pyfits
 from astropy.time import Time
 from pkg_resources import resource_filename
+from surveysim.utils import radec2altaz, mjd2lst
+from operator import itemgetter
 
 Lat_KPNO_deg = 31.0 + (57.0 + 50.3/60.0)/60.0
 
 class surveyPlan:
-
+    
     def __init__(self):
     # Read list of DESI tiles
     # Format of prototype file:
@@ -69,7 +71,92 @@ class surveyPlan:
                 self.LSTmax[i] -= 360.0
 
         self.status = np.zeros(len(self.tileID))
-        self.priority = np.ones(len(self.tileID))
+        self.priority = np.zeros(len(self.tileID))
+        # Assign priority only as a function of DEC
+        for i in range(len(self.priority)):
+            dec = self.DEC[i]
+            if dec <= -15.0:
+                priority = 1
+            elif dec > -15.0 and dec <= 0.0:
+                priority = 2
+            elif dec > 0.0 and dec <= 15.0:
+                priority = 3
+            elif dec > 15.0 and dec <= 30.0:
+                priority = 4
+            elif dec > 30.0 and dec <= 45.0:
+                priority = 5
+            elif dec > 45.0 and dec <= 60.0:
+                priority = 6
+            elif dec > 60.0 and dec < 75.0:
+                priority = 7
+            else:
+                priority = 8
+            self.priority[i] = priority
+
+        # Dummy initialization: this is just to declare the class members
+        self.moonUp = 'BRIGHT'
+        self.LST_dark1_begin = -1.0
+        self.LST_dark1_end = -1.0
+        self.LST_dark2_begin = -1.0
+        self.LST_dark2_end = -1.0
+
+    # Checks whether that part of the night is dark or not.
+    def isItDark(self, lst):
+        if ( (self.LST_dark1_begin > 0.0 and self.LST_dark1_begin < lst and
+              self.LST_dark1_end > 0.0 and lst < self.LST_dark1_end) or
+             (self.LST_dark2_begin > 0.0 and self.LST_dark2_begin < lst and
+              self.LST_dark2_end > 0.0 and lst < self.LST_dark2_end) ):
+            brightness = 'DARK'
+        else:
+            brightness = self.moonUp
+        return brightness
+
+    # Sets the dark times times for this night and determines whether the rest is bright or grey.
+    def getDarkTimes(self, day_stats):
+        self.LST_dark2_begin = -1.0
+        self.LST_dark2_end = -1.0
+        if day_stats['MJDmoonrise'] > 0.0 and day_stats['MJDmoonrise'] > 0.0: # Moon rises and sets tonight
+            if day_stats['MJDmoonrise'] < day_stats['MJDetwi']: # Moon is up by the end of twilight
+                if day_stats['MJDmoonset'] < day_stats['MJDmtwi']: # Moon sets during the night
+                    self.LST_dark1_begin = mjd2lst(day_stats['MJDmoonset'])
+                    self.LST_dark1_end = mjd2lst(day_stats['MJDmtwi'])
+                else: # Moon is always up
+                    self.LST_dark1_begin = -1.0
+                    self.LST_dark1_end = -1.0
+            else: # Moon is down at the end of twilight
+                self.LST_dark1_begin = mjd2lst(day_stats['MJDetwi'])
+                if day_stats['MJDmoonrise'] < day_stats['MJDmtwi']:
+                    self.LST_dark1_end = mjd2lst(day_stats['MJDmoonrise'])
+                    if day_stats['MJDmoonset'] < day_stats['MJDmtwi']: # Second portion of dark time.
+                        self.LST_dark2_begin = mjd2lst(day_stats['MJDmoonset'])
+                        self.LST_dark2_end = mjd2lst(day_stats['MJDmtwi'])
+                else:
+                    self.LST_dark1_end = mjd2lst(day_stats['MJDmtwi'])
+        elif day_stats['MJDmoonrise'] > 0.0 and day_stats['MJDmoonrise'] < 0.0: # Moon rises, but does not set
+            if day_stats['MJDmoonrise'] < day_stats['MJDetwi']: # Moon is already up at end of twilight
+                self.LST_dark1_begin = -1.0
+                self.LST_dark1_end = -1.0
+            else:
+                self.LST_dark1_begin = day_stats['MJDetwi']
+                self.LST_dark1_end = mjd2lst(day_stats['MJDmoonrise'])
+        elif day_stats['MJDmoonrise'] < 0.0 and day_stats['MJDmoonrise'] > 0.0: # Moon doesn't rise, but sets
+            if day_stats['MJDmoonset'] > 0.0 and day_stats['MJDmoonset'] < day_stats['MJDmtwi']:
+                self.LST_dark1_begin = mjd2lst(day_stats['MJDmoonset'])
+                self.LST_dark1_end = mjd2lst(day_stats['MJDmtwi'])
+            else:
+                self.LST_dark1_begin = -1.0
+                self.LST_dark1_end = -1.0
+        else: # Moon doesn't rise or set during the night
+            self.LST_dark_begin = mjd2lst(day_stats['MJDetwi'])
+            self.LST_dark_end = mjd2lst(day_stats['MJDmtwi'])
+        # Just use the elevation at local midnight:
+        # will need to get elevation as a function of time.
+        alt, az = radec2altaz(day_stats['MoonRA'], day_stats['MoonDEC'], mjd2lst(day_stats['MJDmidnight']))
+        if day_stats['MoonFrac'] < 0.2 or (alt*day_stats['MoonFrac'] < 12.0):
+            self.moonUp = 'GREY'
+        else:
+            self.moonUp = 'BRIGHT'
+
 
     def afternoonPlan(self, day_stats, tiles_observed):
         """
@@ -78,47 +165,27 @@ class surveyPlan:
         name has format obsplanYYYYMMDD.fits .
         """
 
+        # Update status
         nto = len(tiles_observed)
         for i in range(nto):
             j = np.where(self.tileID == tiles_observed['TILEID'][i])
             self.status[j] = tiles_observed['STATUS'][i]
-        
-        # table: ra, dec, tileid, priority, design HA, estimated exp time, LST begin/end and or min/max, templates, programme name, S/N2
-        # FOR NOW RETURN ALL UNOBSERVED TILES
-        a1 = self.tileID.compress((self.status<2).flat)
-        col1 = pyfits.Column(name='TILEID', format='J', array=a1)
 
-        a2 = self.RA.compress((self.status<2).flat)
-        col2 = pyfits.Column(name='RA', format='D', array=a2)
+        self.getDarkTimes(day_stats)
+        planList0 = []
 
-        a3 = self.DEC.compress((self.status<2).flat)
-        col3 = pyfits.Column(name='DEC', format='D', array=a3)
+        for i in range(len(self.tileID)):
+            if ( self.status[i] < 2 and
+                ((self.isItDark(self.LSTmin[i]) == 'DARK' and self.isItDark(self.LSTmax[i]) == 'DARK' and self.program[i] == 'DARK') or
+                 ((self.isItDark(self.LSTmin[i]) != 'DARK' or self.isItDark(self.LSTmax[i]) != 'DARK') and self.program[i] != 'DARK')) ):
+                planList0.append((self.tileID[i], self.RA[i], self.DEC[i], self.Ebmv[i], self.LSTmin[i], self.LSTmax[i],
+                                 self.maxExpLen[i], self.priority[i], self.status[i], self.program[i], self.obsconds[i]))
 
-        a4 = self.Ebmv.compress((self.status<2).flat)
-        col4 = pyfits.Column(name='EBV_MED', format='E', array=a4)
+        planList = sorted(planList0, key=itemgetter(7), reverse=True)
+        cols = np.rec.array(planList,
+                            names = ('TILEID', 'RA', 'DEC', 'EBV_MED', 'LSTMIN', 'LSTMAX', 'MAXEXPLEN', 'PRIORITY', 'STATUS', 'PROGRAM', 'OBSCONDITIONS'),
+                            formats = ['i4', 'f8', 'f8', 'f8', 'f4', 'f4', 'f4', 'i4', 'i4', 'a6', 'i2'])
 
-        a5 = self.LSTmin.compress((self.status<2).flat)
-        col5 = pyfits.Column(name='LSTMIN', format='E', array=a5)
-
-        a6 = self.LSTmax.compress((self.status<2).flat)
-        col6 = pyfits.Column(name='LSTMAX', format='E', array=a6)
-
-        a7 = self.maxExpLen.compress((self.status<2).flat)
-        col7 = pyfits.Column(name='MAXEXPLEN', format='E', array=a7)
-
-        a8 = self.priority.compress((self.status<2).flat)
-        col8 = pyfits.Column(name='PRIORITY', format='J', array=a8)
-        
-        a9 = self.status.compress((self.status<2).flat)
-        col9 = pyfits.Column(name='STATUS', format='J', array=a9)
-
-        a10 = self.program.compress((self.status<2).flat)
-        col10 = pyfits.Column(name='PROGRAM', format='6A', array=a10)
-
-        a11 = self.obsconds.compress((self.status<2).flat)
-        col11 = pyfits.Column(name='OBSCONDITIONS', format='I', array=a11)
-
-        cols = pyfits.ColDefs([col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11])
         tbhdu = pyfits.BinTableHDU.from_columns(cols)
 
         prihdr = pyfits.Header()
@@ -126,16 +193,11 @@ class surveyPlan:
         prihdr['MOONRA  '] = day_stats['MoonRA']
         prihdr['MOONDEC '] = day_stats['MoonDEC']
         prihdu = pyfits.PrimaryHDU(header=prihdr)
-
-        t = Time(day_stats['MJDsunset'], format = 'mjd')
-        utc = t.iso
         filename = 'obsplan' + day_stats['dirName'] + '.fits'
         thdulist = pyfits.HDUList([prihdu, tbhdu])
         thdulist.writeto(filename, clobber=True)
 
-        tilesTODO = len(a9)
+        tilesTODO = len(planList)
 
         return tilesTODO, filename
 
-        
-    

py/surveysim/exposurecalc.py
@@ -1,12 +1,7 @@
 import numpy as np
 from astropy.time import Time
 from surveysim.weather import weatherModule
-
-# Location of Kitt Peak National Observatory Mayall 4m Telescope
-# Taken from SlaLib obs.c, which cites the 1981 Almanac
-Lon_KPNO_deg = -1.0 * (111.0 + (35.0 + 57.61/60.0)/60.0)
-Lat_KPNO_deg = 31.0 + (57.0 + 50.3/60.0)/60.0
-Alt_KPNO_m   = 2120.0
+from surveysim.utils import radec2altaz
 
 def expTimeEstimator(weatherNow, airmass, program, ebmv, sn2, moonFrac):
 # Estimates expusure length given current conditions.
@@ -14,12 +9,15 @@ def expTimeEstimator(weatherNow, airmass, program, ebmv, sn2, moonFrac):
     seeing_ref = 1.1 # Seeing value to which actual seeing is normalised
     exp_ref_dark = 1000.0   # Reference exposure time in seconds
     exp_ref_bright = 300.0  # Idem but for bright time programme
+    exp_ref_grey = 650.0    # Made up number: just took the average
     sn2_nom = 100.0 # Nominal sign-to-noise
 
     if program == "DARK":
         exp_ref = exp_ref_dark
     elif program == "BRIGHT":
         exp_ref = exp_ref_bright
+    elif program == "GRAY":
+        exp_ref = exp_ref_grey
     else:
         exp_ref = 0.0 # Replace with throwing an exception
     seeing = weatherNow['Seeing']
@@ -32,8 +30,7 @@ def expTimeEstimator(weatherNow, airmass, program, ebmv, sn2, moonFrac):
         f_transparency = 1.0 / weatherNow['Transparency']
     else:
         f_transparency = 1.0e9
-    f_airmass = np.sqrt(airmass)
-    f_ebmv = np.exp(-ebmv) # What's the correct factor?
+    f_ebmv = np.power(10.0,ebmv/2.5)
     """
     if moonFrac < 1.0:
         f_moon = 1.0 / (1.0 - moonFrac/100.0)
@@ -41,7 +38,7 @@ def expTimeEstimator(weatherNow, airmass, program, ebmv, sn2, moonFrac):
         f_moon = 30.0
     """
     f_moon = 1.0 # Temporary until real values are in the code
-    f = f_seeing * f_transparency * f_airmass * f_ebmv * f_moon
+    f = f_seeing * f_transparency * f_ebmv * f_moon
     if f >= 0.0:
         value = exp_ref * f * (sn2 / sn2_nom)
     else:
@@ -49,19 +46,10 @@ def expTimeEstimator(weatherNow, airmass, program, ebmv, sn2, moonFrac):
     return value
 
 def airMassCalculator(ra, dec, lst): # Valid for small to moderate angles.
-    # RA and LST are in decimal hours, DEC in decimal degrees
-    # Note that these are *observed* RA and DEC, not mean, not apparent.
-    h = np.radians(lst - ra)
-    if h < 0.0:
-        h += 360.0
-    d = np.radians(dec)
-    phi = np.radians(Lat_KPNO_deg)
-
-    # sinAz = np.sin(h) / (np.cos(h)*np.sin(phi) - np.tan(d)*np.cos(phi))
-    sina = np.sin(phi)*np.sin(d) + np.cos(phi)*np.cos(d)*np.cos(h)
-
-    amass = 1.0/sina
+    Alt, Az = radec2altaz(ra, dec, lst)
+    # Rosenberg (1966) formula
+    cosZ = np.cos(np.radians(90.0-Alt))
+    amass = 1.0/(cosZ + 0.025*np.exp(-11.0*cosZ))
     if amass <= 0.0:
         print ('ERROR: negative airmass (', amass, '); LST, RA, DEC = ', lst, ra, dec,'!')
     return amass
-

py/surveysim/nextobservation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import astropy.io.fits as pyfits
+from surveysim.utils import angsep
 
 # This is a VERY simplfied version of the next field seclector.
 # The only thing it does is return the first target on the list
@@ -24,11 +25,18 @@ def nextFieldSelector(obsplan, lst, conditions, tilesObserved):
     i = 0
     for t1 in np.nditer(tmin):
         t2 = tmax[i] - explen[i]
-        if (lst > t1 and lst < t2
-            and abs(ra[i] - moonRA) > MIN_MOON_SEP and abs(dec[i] - moonDEC) > MIN_MOON_SEP):
-            if ( (len(tilesObserved) > 0 and tileID[i] not in tilesObserved['TILEID']) or len(tilesObserved) == 0 ):
-                found = True
-                break
+        if t1 < t2:
+            if (lst > t1 and lst < t2
+                and angsep(moonRA, moonDEC, ra[i], dec[i]) > MIN_MOON_SEP):
+                if ( (len(tilesObserved) > 0 and tileID[i] not in tilesObserved['TILEID']) or len(tilesObserved) == 0 ):
+                    found = True
+                    break
+        else:
+            if ( ((lst > t1 and t1 <=360.0) or (lst >= 0.0 and lst < t2))
+                 and angsep(moonRA, moonDEC, ra[i], dec[i]) > MIN_MOON_SEP):
+                if ( (len(tilesObserved) > 0 and tileID[i] not in tilesObserved['TILEID']) or len(tilesObserved) == 0 ):
+                    found = True
+                    break
         i = i+1
 
     if found == True: