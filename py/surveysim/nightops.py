291994439efb8e1da600bd348bd45ee26c0b2623
David Kirkby <dkirkby@uci.edu>
2017-05-05 17:08:38 -0700

    Replace hardcoded params with config.xxx() in nightOps

--- py/surveysim/nightops.py
+++ py/surveysim/nightops.py
@@ -15,12 +15,6 @@ import desisurvey.ephemerides
 import desisurvey.config
 
 
-LSTres = 1.0/144.0 # Should be the same as in afternoon planner and next field selector
-MaxExpLen = 3600.0 # One hour
-CRsplit = 1200.0   # 20 minutes
-ReadOutTime = 120.0 # Should be the same as in next field selector
-
-
 def nightOps(night, obsplan, weather, progress, gen):
     """Simulate one night of observing.
 
@@ -58,6 +52,9 @@ def nightOps(night, obsplan, weather, progress, gen):
     slew = False
     ra_prev = 1.0e99
     dec_prev = 1.0e99
+    # How long to delay when we don't have a suitable target to observe.
+    delay = 1. / config.num_lst_bins()
+
     while mjd < night['brightdawn']:
         # Get the current weather conditions.
         conditions = weather.get(time)
@@ -65,12 +62,13 @@ def nightOps(night, obsplan, weather, progress, gen):
         # Select the next target to observe.
         target, overhead = desisurvey.nextobservation.nextFieldSelector(
             obsplan, mjd, conditions, progress, slew, ra_prev, dec_prev)
+        overhead = overhead * u.s
         if target is None:
             # Wait until a target is available.
-            mjd += LSTres
+            mjd += delay
             slew = False
             continue
-        log.debug('Selected {0} tile {1} at MJD {2:.5f} with {3:.1f}s overhead.'
+        log.debug('Selected {0} tile {1} at MJD {2:.5f} with {3:.1f} overhead.'
                   .format(target['Program'], target['tileID'], mjd, overhead))
         # Calculate the target's airmass.
         airmass = desisurvey.utils.get_airmass(
@@ -80,29 +78,31 @@ def nightOps(night, obsplan, weather, progress, gen):
         total_exptime = desisurvey.exposurecalc.expTimeEstimator(
             seeing, transparency, airmass, target['Program'], target['Ebmv'],
             target['DESsn2'], night['moon_illum_frac'], target['MoonDist'],
-            target['MoonAlt'])
+            target['MoonAlt']) * u.s
         # Calculate the target exposure time for this observation.
         # Should account for previous exposure time of partial targets here.
         target_exptime = total_exptime
         # Is this target worth observing now?
-        if target_exptime > MaxExpLen:
-            log.debug('Best target requires {0:.1f}s exposure.  Waiting...'
+        if target_exptime > config.max_exposure_length():
+            log.debug('Best target requires {0:.1f} exposure.  Waiting...'
                       .format(target_exptime))
-            mjd += LSTres
+            mjd += delay
             slew = False # Can slew to new target while waiting.
             continue
         # Add random jitter with 10% RMS to the actual exposure time.
         exptime = target_exptime * (1 + gen.normal(scale=0.1))
         # Determine what fraction of the SNR target we have reached with
         # this exposure.
-        snrfrac = exptime / total_exptime
+        snrfrac = (exptime / total_exptime).to(1).value
         # Record this exposure.
         progress.add_exposure(
-            target['tileID'], mjd, exptime, snrfrac, airmass, seeing)
+            target['tileID'], mjd, exptime.to(u.s).value,
+            snrfrac, airmass, seeing)
         # Add extra readout time for cosmic-ray splits, if necessary.
-        overhead += ReadOutTime * np.floor(exptime / CRsplit)
+        nexp = int(np.floor((exptime / config.cosmic_ray_split()).to(1).value))
+        overhead += nexp * config.readout_time()
         # Prepare for the next exposure.
-        mjd += (overhead + exptime)/86400.0
+        mjd += (overhead + exptime).to(u.day).value
         slew = True
         ra_prev = target['RA']
         dec_prev = target['DEC']
