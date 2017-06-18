394635f9a21246c92bc800032751235b30b2ea8b
David Kirkby <dkirkby@uci.edu>
2017-06-18 11:15:07 -0700

    Fix unit tests

--- py/surveysim/test/test_surveysim.py
+++ py/surveysim/test/test_surveysim.py
@@ -8,6 +8,8 @@ import os
 
 import numpy as np
 
+import astropy.table
+
 import desisurvey.config
 import desisurvey.progress
 
@@ -37,14 +39,23 @@ class TestSurveySim(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_surveysim(self):
+    def test_simulator(self):
         gen = np.random.RandomState(123)
         start = datetime.date(2019,9,1)
         stop = datetime.date(2019,9,8)
         progress = desisurvey.progress.Progress()
         weather = surveysim.weather.Weather(start, stop, gen=gen)
+        # Initialize a table for efficiency stats tracking.
+        stats = astropy.table.Table()
+        config = desisurvey.config.Configuration()
+        num_nights = (config.last_day() - config.first_day()).days
+        stats['available'] = np.zeros(num_nights)
+        stats['overhead'] = np.zeros(num_nights)
+        stats['delay'] = np.zeros(num_nights)
+        stats['dawn'] = np.zeros(num_nights)
+        stats['live'] = np.zeros(num_nights)
         sim = surveysim.simulator.Simulator(
-            start, stop, progress, weather, gen=gen)
+            start, stop, progress, weather, stats, gen=gen)
         while sim.next_day():
             pass
 
