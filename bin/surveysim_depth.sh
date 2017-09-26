f356fbb1c74c023922a24344ff62d6600a8cf05b
David Kirkby <dkirkby@uci.edu>
2017-09-26 16:22:33 -0700

    Add new depth-first simulation driver

---
+++ bin/surveysim_depth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#############################################################################
+# Simulate the baseline survey strategy described in DESI-doc-1767-v3.
+# Note that this is one random realization of the observing conditions.
+# Change the random seed for a different realization.
+# This will take ~4 hours to run and writes ~5.1G to $DESISURVEY.
+# For a visualization of this simulation (created with surveymovie) see
+# https://www.youtube.com/watch?v=vO1QZD_aCIo
+#############################################################################
+
+PLAN_ARGS='--verbose --fa-delay 1m --rules rules-depth.yaml'
+SIM_ARGS='--verbose --scores --seed 123 --strategy HA+fallback'
+
+surveyinit --verbose
+surveyplan --create ${PLAN_ARGS}
+surveysim ${SIM_ARGS}
+
+while :
+do
+    (surveyplan ${PLAN_ARGS}) || break
+    (surveysim --resume ${SIM_ARGS}) || break
+done
