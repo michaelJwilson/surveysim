7db901026ad525a25eb3077d19715d3bcaff3540
Benjamin Alan Weaver <baweaver@lbl.gov>
2017-11-09 11:05:57 -0700

    make conversion function less sensitive to column order

--- py/surveysim/test/test_util.py
+++ py/surveysim/test/test_util.py
@@ -1,9 +1,13 @@
+"""Test surveysim.util.
+"""
 import unittest
 import numpy as np
 from astropy.table import Table, Column
 from ..util import add_calibration_exposures
 
 class TestUtil(unittest.TestCase):
+    """Test surveysim.util.
+    """
 
     @classmethod
     def setUpClass(cls):
@@ -20,6 +24,8 @@ class TestUtil(unittest.TestCase):
         pass
 
     def test_add_calibration_exposures(self):
+        """Test adding calibration exposures to science exposures.
+        """
         exposures = Table()
         exposures['TILEID'] = Column(np.array([0, 1], dtype=np.int32))
         exposures['PASS'] = Column(np.array([0, 0], dtype=np.int16))
@@ -27,7 +33,7 @@ class TestUtil(unittest.TestCase):
         exposures['DEC'] = Column(np.array([0.0, 1.0], dtype=np.float64))
         exposures['EBMV'] = Column(np.array([0.0, 1.0], dtype=np.float64))
         exposures['NIGHT'] = Column(np.array(['20200101', '20200102'], dtype=(str, 8)))
-        exposures['MJD'] = Column(np.array([0.0, 1.0], dtype=np.float64))
+        exposures['MJD'] = Column(np.array([58849.0, 58850.0], dtype=np.float64))
         exposures['EXPTIME'] = Column(np.array([0.0, 1.0], dtype=np.float64), unit='s')
         exposures['SEEING'] = Column(np.array([0.0, 1.0], dtype=np.float64), unit='arcsec')
         exposures['TRANSPARENCY'] = Column(np.array([0.0, 1.0], dtype=np.float64))
@@ -42,3 +48,11 @@ class TestUtil(unittest.TestCase):
         self.assertEqual('EXPID', output.colnames[0])
         self.assertTrue(np.all(output['EXPID'] == np.arange(14, dtype=np.int32)))
         self.assertEqual(output['RA'].unit, 'deg')
+        print(output['MJD'])
+
+def test_suite():
+    """Allows testing of only this module with the command::
+
+        python setup.py test -m <modulename>
+    """
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
--- py/surveysim/util.py
+++ py/surveysim/util.py
@@ -5,7 +5,9 @@ import numpy as np
 from astropy.table import Column
 
 
-def add_calibration_exposures(exposures, arcs_per_night=3, flats_per_night=3):
+def add_calibration_exposures(exposures, flats_per_night=3, arcs_per_night=3,
+                              darks_per_night=0, zeroes_per_night=0,
+                              exptime=10.0, readout=30.0):
     """Adds calibration exposures to a set of science exposures and
     assigns exposure IDs.
 
@@ -14,10 +16,18 @@ def add_calibration_exposures(exposures, arcs_per_night=3, flats_per_night=3):
     exposures : :class:`astropy.table.Table`
         A table of science exposures, produced by *e.g.*
         ``desisurvey.progress.Progress.get_exposures()``.
-    arcs_per_night : :class:`int`, optional
-        Add this many arc exposures per night (default 3).
     flats_per_night : :class:`int`, optional
         Add this many arc exposures per night (default 3).
+    arcs_per_night : :class:`int`, optional
+        Add this many arc exposures per night (default 3).
+    darks_per_night : :class:`int`, optional
+        Add this many dark exposures per night (default 0).
+    zeroes_per_night : :class:`int`, optional
+        Add this many zero exposures per night (default 0).
+    exptime : :class:`float`, optional
+        Set calibration exposure times (default 10.0 s).
+    readout : :class:`float`, optional
+        Set readout time for calibration exposures (default 30.0 s).
 
     Returns
     -------
@@ -34,18 +44,41 @@ def add_calibration_exposures(exposures, arcs_per_night=3, flats_per_night=3):
     output = exposures[:0].copy()
     current_night = '19730703'
     expid = 0
+    calib_exptime = lambda x: 0.0 if x == 'zero' else exptime
+    calib_time = lambda x: calib_exptime(x) + readout
+    calib_sequence = (['flat']*flats_per_night + ['arc']*arcs_per_night +
+                      ['dark']*darks_per_night + ['zero']*zeroes_per_night)
+    calib_times = np.cumsum(np.array([calib_time(c)
+                                      for c in calib_sequence])[::-1])
+    # None fields get filled in for each calibration exposure.
+    calib_data = {'EXPID': None,
+                  'TILEID': -1,
+                  'PASS': -1,
+                  'RA': 0.0,
+                  'DEC': 0.0,
+                  'EBMV': 0.0,
+                  'NIGHT': None,
+                  'MJD': None,
+                  'EXPTIME': None,
+                  'SEEING': 0.0,
+                  'TRANSPARENCY': 0.0,
+                  'AIRMASS': 0.0,
+                  'MOONFRAC': 0.0,
+                  'MOONALT': 0.0,
+                  'MOONSEP': 0.0,
+                  'PROGRAM': 'CALIB',
+                  'FLAVOR': None
+                  }
     for i, night in enumerate(exposures['NIGHT']):
         if night != current_night:
             current_night = night
-            for j in range(flats_per_night):
-                output.add_row([expid, -1, -1, 0.0, 0.0, 0.0, night,
-                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                'NONE', 'flat'])
-                expid += 1
-            for j in range(arcs_per_night):
-                output.add_row([expid, -1, -1, 0.0, 0.0, 0.0, night,
-                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                'NONE', 'arc'])
+            for j, c in enumerate(calib_sequence):
+                calib_data['EXPID'] = expid
+                calib_data['NIGHT'] = night
+                calib_data['MJD'] = exposures['MJD'][i] - calib_times[j]/86400.0
+                calib_data['EXPTIME'] = calib_exptime(c)
+                calib_data['FLAVOR'] = c
+                output.add_row(calib_data)
                 expid += 1
         output.add_row(exposures[i])
         output['EXPID'][expid] = expid
