a3d35e6b733e652512db5e981c6e1a3df188788b
David Kirkby <dkirkby@uci.edu>
2018-10-13 12:05:51 -0700

    ExposureList uses Tiles instead of Scheduler

--- py/surveysim/exposures.py
+++ py/surveysim/exposures.py
@@ -4,11 +4,13 @@ from __future__ import print_function, division, absolute_import
 
 import numpy as np
 
+import desisurvey.tiles
+
 
 class ExposureList(object):
 
-    def __init__(self, scheduler, max_nexp=50000):
-        self.scheduler = scheduler
+    def __init__(self, tiles_file=None, max_nexp=50000):
+        self.tiles = desisurvey.tiles.get_tiles(tiles_file)
         self._exposures = np.empty(max_nexp, dtype=[
             ('mjd', np.float64),
             ('exptime', np.float32),
@@ -19,18 +21,17 @@ class ExposureList(object):
             ('seeing', np.float32),
             ('transp', np.float32),
         ])
-        ntiles = len(self.scheduler.tileid)
-        self._tiledata = np.empty(ntiles, dtype=[
+        self._tiledata = np.empty(self.tiles.ntiles, dtype=[
             ('exptime', np.float32),
             ('snr2frac', np.float32),
             ('nexp', np.int32)
         ])
         self.reset()
-        
+
     def reset(self):
         self.nexp = 0
         self._tiledata[:] = 0
-        
+
     def add(self, mjd, exptime, tileid, passnum, snr2frac_start, snr2frac_stop,
             seeing, transp):
         if self.nexp >= len(self._exposures):
@@ -40,7 +41,7 @@ class ExposureList(object):
             mjd, exptime, tileid, passnum, snr2frac_start, snr2frac_stop,
             seeing, transp)
         self.nexp += 1
-        tileinfo = self._tiledata[self.scheduler.get_tile_index(tileid)]
+        tileinfo = self._tiledata[self.tiles.index(tileid)]
         tileinfo['exptime'] += exptime
         tileinfo['snr2frac'] = snr2frac_stop
         tileinfo['nexp'] += 1
